3d49019d16fbbedf3276d324b988656212cc1e52
rwilgaard <rasmus@wilgaard.dk>
2023-02-20 20:30:31 +0100

    feat: Expose Branches API

---
+++ branch.go
@@ -0,0 +1,83 @@
+package gobitbucket
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type Branch struct {
+    Default         bool   `json:"default,omitempty"`
+    Type            any    `json:"type,omitempty"`
+    DisplayID       string `json:"displayId,omitempty"`
+    LatestCommit    string `json:"latestCommit,omitempty"`
+    LatestChangeset string `json:"latestChangeset,omitempty"`
+    ID              string `json:"id"`
+}
+
+type BranchList struct {
+    Values        []*Branch `json:"values"`
+    Size          int       `json:"size"`
+    Limit         int       `json:"limit"`
+    Start         int32     `json:"start"`
+    IsLastPage    bool      `json:"isLastPage"`
+    NextPageStart int32     `json:"nextPageStart"`
+}
+
+type BranchesQuery struct {
+    ProjectKey     string
+    RepositorySlug string
+    BoostMatches   bool   // Controls whether exact and prefix matches will be boosted to the top
+    OrderBy        string // Ordering of refs either ALPHABETICAL (by name) or MODIFICATION (last updated)
+    Details        bool   // Whether to retrieve plugin-provided metadata about each branch
+    FilterText     string // The text to match on
+    Base           string // Base branch or tag to compare each branch to (for the metadata providers that uses that information
+    Start          int
+    Limit          int
+}
+
+func getBranchesQueryParams(query BranchesQuery) *url.Values {
+    data := url.Values{}
+    if query.BoostMatches {
+        data.Set("boostMatches", "true")
+    }
+    if query.OrderBy != "" {
+        data.Set("orderBy", query.OrderBy)
+    }
+    if query.Details {
+        data.Set("details", "true")
+    }
+    if query.FilterText != "" {
+        data.Set("filterText", query.FilterText)
+    }
+    if query.Base != "" {
+        data.Set("base", query.Base)
+    }
+    if query.Start != 0 {
+        data.Set("start", strconv.Itoa(query.Start))
+    }
+    if query.Limit != 0 {
+        data.Set("limit", strconv.Itoa(query.Limit))
+    }
+    return &data
+}
+
+func (a *API) GetBranches(query BranchesQuery) (*BranchList, *http.Response, error) {
+    params := getBranchesQueryParams(query)
+    path := fmt.Sprintf("/rest/api/latest/projects/%s/repos/%s/branches", query.ProjectKey, query.RepositorySlug)
+    req, err := a.NewRequest("GET", path, nil, params)
+    if err != nil {
+        return nil, nil, err
+    }
+
+    branches := BranchList{
+        IsLastPage: true,
+    }
+    resp, err := a.Do(req, &branches)
+    if err != nil {
+        return nil, resp, err
+    }
+
+    return &branches, resp, err
+}
